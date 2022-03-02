bdc3b33495a01d93e92dc2f51589b5397d10fb0a
kimyoosang <yoo99485@gmail.com>
2022-03-02 19:30:26 +0900

    [Add] typscript & tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <main>
+        <h1>Next Shop</h1>
+      </main>
+    </>
+  );
+}
+
+export default Home;
