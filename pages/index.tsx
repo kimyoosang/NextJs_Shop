30c95cc96667c71c6a6249a63bf8c86a23a71891
kimyoosang <yoo99485@gmail.com>
2022-03-28 17:44:40 +0900

    [Add] ProductCard Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,14 +30,14 @@ const HomePage: React.FC<HomePageProps> = ({ products }) => {
       </Head>
       <main className="px-6 py-4">
         <Title>Next Shop</Title>
+        <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+          {products.map((product) => (
+            <li key={product.id}>
+              <ProductCard product={product} />
+            </li>
+          ))}
+        </ul>
       </main>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <ProductCard product={product} />
-          </li>
-        ))}
-      </ul>
     </>
   );
 };
