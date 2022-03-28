1c990a0c55a4ad32dcb96cb8a24fc01c42deec31
kimyoosang <yoo99485@gmail.com>
2022-03-28 18:06:10 +0900

    [Update] ProductCard Image link & eddit next.config/js

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,17 +1,18 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { Product } from '../lib/product';
 
-interface ProductCardPage {
+interface ProductCardProps {
   product: Product;
 }
 
-const ProductCard: React.FC<ProductCardPage> = ({ product }) => {
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
     <div className="border w-80 shadow hover:shadow-xl">
       <Link href={`/products/${product.id}`}>
         <a>
-          <img src="https://dummyimage.com/320x240" alt="dummyimage" />
+          <Image src={product.pictureUrl} alt="" width={320} height={240} />
           <div className="p-2 flex justify-between items-baseline">
             <h2 className="text-lg font-bold">{product.title}</h2>
             <span>{product.price}</span>
