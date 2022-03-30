55cadc2ce29223abb2449717a397c87ee1f6dce6
kimyoosang <yoo99485@gmail.com>
2022-03-30 21:56:49 +0900

    [Add] user hooks

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { FormEventHandler, useState } from 'react';
+import Field from '../components/Field';
+import Input from '../components/Input';
 import Page from '../components/Page';
 
 const SigninPage: React.FC = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+  };
   return (
     <Page title="Sign In">
-      <form></form>
+      <form>
+        <Field label="Email">
+          <Input
+            type={'email'}
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Field>
+        <Field label="Password">
+          <Input
+            type={'password'}
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Field>
+      </form>
     </Page>
   );
 };
