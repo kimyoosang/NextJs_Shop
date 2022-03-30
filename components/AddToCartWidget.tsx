66baa34674174260ca31be703fca4e2a34b89801
kimyoosang <yoo99485@gmail.com>
2022-03-31 02:05:18 +0900

    [Add] AddToCartWidget component

---
+++ components/AddToCartWidget.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface AddToCartWidgetProps {
+  productId: number;
+}
+const AddToCartWidget: React.FC<AddToCartWidgetProps> = ({ productId }) => {
+  return <div>AddToCartWidget</div>;
+};
+
+export default AddToCartWidget;
