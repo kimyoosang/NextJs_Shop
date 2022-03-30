678799f8f8e02c0446825e347ae0475fd3a68314
kimyoosang <yoo99485@gmail.com>
2022-03-30 21:07:15 +0900

    [Add] Field component

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Page from '../components/Page';
 
 const SigninPage: React.FC = () => {
-  return <div>sign-in</div>;
+  return (
+    <Page title="Sign In">
+      <form></form>
+    </Page>
+  );
 };
 
 export default SigninPage;
