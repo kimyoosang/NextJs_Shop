d266627ca7b1d6c608ee05c7898c770de20b0a9c
kimyoosang <yoo99485@gmail.com>
2022-03-31 02:19:05 +0900

    [Add] cart api

--- components/AddToCartWidget.tsx
+++ components/AddToCartWidget.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 
 interface AddToCartWidgetProps {
   productId: number;
 }
 const AddToCartWidget: React.FC<AddToCartWidgetProps> = ({ productId }) => {
-  return <div>AddToCartWidget</div>;
-};
+  const router = useRouter();
+  const [quantity, setQuantity] = useState(1);
 
+  return (
+    <div className="py-2">
+      <input
+        type="number"
+        min={1}
+        className="border rounded px-3 py-1 mr-2 w-16 text-right"
+        value={quantity.toString()}
+        onChange={(e) => setQuantity(parseInt(e.target.value))}
+      />
+    </div>
+  );
+};
 export default AddToCartWidget;
